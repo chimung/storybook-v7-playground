9aabb0364e36874ef8f5263732a3b2c7d1d56efb
chimung <cungthehung@gmail.com>
2023-06-06 04:15:44 +0000

    Update Button.stories.js

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,9 +1,9 @@
-import { Button } from './Button';
+import { Button as ButtonComponent } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
   title: 'Example/Button',
-  component: Button,
+  component: ButtonComponent,
   tags: ['autodocs'],
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -11,29 +11,9 @@ export default {
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary = {
+export const Button = {
   args: {
     primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small = {
-  args: {
-    size: 'small',
     label: 'Button',
   },
 };
